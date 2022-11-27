67551154d95f0a5983200a415bb512b3d9202a33
Nickolay-Klimentov <moody.klmn@icloud.com>
2022-11-27 14:52:34 +0300

    solve 1-5 board

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,100 @@
+// функция формирующая из строки массив с игровым полем
+function getBoard(boardString) {
+  const board = [];
+  for (let i = 0; i < boardString.length; i += 9) {
+    const row = boardString.slice(i, i + 9);
+    board.push(row.split(''));
+  }
+  return board;
+}
+
+// функция проверки наличия пустых ячеек на поле
+function checkEmpty(board) {
+  for (let i = 0; i < 9; i += 1) {
+    for (let j = 0; j < 9; j += 1) {
+      if (board[i][j] === '-') {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// функция проверяющая возможные решения
+function checkSolve(position, board) {
+  const [i, j] = position;
+  const numbers = {
+    1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+  };
+  // проверка по строке
+  for (let col = 0; col < board.length; col += 1) {
+    const number = board[i][col];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in numbers) {
+      if (key === number) {
+        delete numbers[key];
+      }
+    }
+  }
+  // проверка по столбцу
+  for (let row = 0; row < board.length; row += 1) {
+    const number = board[row][j];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in numbers) {
+      if (key === number) {
+        delete numbers[key];
+      }
+    }
+  }
+  // проверка по блоку 3х3
+  const miniRow = Math.floor(i / 3) * 3;
+  const miniCol = Math.floor(j / 3) * 3;
+
+  for (let row = miniRow; row < miniRow + 3; row += 1) {
+    for (let col = miniCol; col < miniCol + 3; col += 1) {
+      const number = board[row][col];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const key in numbers) {
+        if (key === number) {
+          delete numbers[key];
+        }
+      }
+    }
+  }
+  return Object.keys(numbers);
+}
+
+// функция решающая судоку
+function getSolve(board) {
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board.length; j += 1) {
+      if (board[i][j] === '-') {
+        const position = [i, j];
+        const numbers = checkSolve(position, board);
+        if (numbers.length === 1) {
+          const [number] = numbers;
+          // eslint-disable-next-line no-param-reassign
+          board[i][j] = number;
+          getSolve(board);
+          if (checkEmpty(board) === false) getSolve(board);
+        }
+      }
+    }
+  }
+  return board;
+}
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const board = getBoard(boardString);
+  getSolve(board);
+  if (checkEmpty(board) === false) getSolve(board);
+  // console.log(board);
+  return board;
 }
 
 /**
@@ -11,7 +102,12 @@ function solve(boardString) {
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-
+  let sum = 0;
+  for (let i = 0; i < board.length; i += 1) {
+    const sumStr = board[i].reduce((a, b) => Number(a) + Number(b));
+    sum += sumStr;
+  }
+  return (sum === 405);
 }
 
 /**
@@ -20,7 +116,25 @@ function isSolved(board) {
  * Подумай, как симпатичнее сформировать эту строку.
  */
 function prettyBoard(board) {
-
+  const solvedBoard = [];
+  for (let j = 0; j < board.length; j += 1) {
+    if (j % 3 === 0) solvedBoard.push('\n');
+    for (let i = 0; i < board[j].length; i += 1) {
+      if (i % 3 === 2) {
+        if (i % 9 === 8) {
+          const result = `${board[j][i]}\n`;
+          solvedBoard.push(result);
+        } else {
+          const result = `${board[j][i]}   `;
+          solvedBoard.push(result);
+        }
+      } else {
+        const result = `${board[j][i]}  `;
+        solvedBoard.push(result);
+      }
+    }
+  }
+  return solvedBoard.join('');
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
